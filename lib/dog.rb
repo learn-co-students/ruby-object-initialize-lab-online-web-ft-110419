d8857006cba3c7016dbd6e70b32d74dda4a3222b
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-13 03:07:41 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,20 +5,27 @@ class Dog
     @breed = breed
   end
 
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-
 end
+
+
+#originally had this thing which was unneccessary
+#for the test:
+
+
+#  def name=(name)
+    #@name = name
+  #end
+
+  #def name
+    #@name
+  #end
+
+  #def breed=(breed)
+    #@breed = breed
+  #end
+
+  #def breed
+    #@breed
+  #end
+
+  #which still passed the test
